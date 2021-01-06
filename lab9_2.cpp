5cb974b756cbf8a5610712810e773f4cad94f242
neokarn <34811101+neokarn@users.noreply.github.com>
2021-01-06 19:01:10 +0700

    Update lab9_2.cpp

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,4 +1,18 @@
-//cout << "-------------------- SOTUS ---------------------";
-//cout << textline;
-//cout << "-------------------- SOTUS ---------------------";
-//Don't forget to change cout to output filestream
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int main (){
+	ifstream source;
+	ofstream dest;
+	source.open("cheerbook.txt") ;
+	dest.open("cheerbook_copy.txt");
+	
+	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
+	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+
+    source.close();
+    dest.close();
+	return 0;
+}
