2070144da6b453b662b98f0e90251c66aa066407
Goodl3oyZ <panudetzx2@gmail.com>
2023-01-03 15:51:03 +0700

    11.2 sedla

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -8,10 +8,11 @@ int main (){
 	ofstream dest;
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
-	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
-
+	dest<<"-------------------- BOOM ---------------------\n";
+	string textline;
+	while(getline(source,textline))
+		dest<< textline<<'\n';
+	dest<<"-------------------- HA!! ---------------------";
     source.close();
     dest.close();
 	return 0;
